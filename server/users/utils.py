9e3e84c1865d265c47be9c4cfa67d7b30d7d146e
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2024-04-14 00:55:25 +0530

    fk this shit

--- server/users/utils.py
+++ server/users/utils.py
@@ -722,51 +722,51 @@ Follow the provided JSON format diligently."""
 
   return output
 
-def generate_content_from_textbook(module_name, vectordb):
-  relevant_docs = vectordb.similarity_search(module_name)
-  rel_docs = [doc.page_content for doc in relevant_docs]
-  context = '\n'.join(rel_docs)
-  print('CONTEXT:\n'+context+'\n\n\n')
+def generate_content_from_textbook(module_name, output, profile, vectordb, api_key_to_use):
+    prompt= """I'm seeking your expertise on the subject of {sub_module_name} which comes under the module: {module_name}.\
+As a knowledgeable educational assistant, I trust in your ability to provide \
+a comprehensive explanation of this sub-module. Think about the sub-module step by step and design the best way to explain the sub-module to me. \
+Your response should cover essential aspects such as definition, in-depth examples, and any details crucial for understanding the topic. \
+You have access to the subject's information which you have to use while generating the educational content. \
+Please generate quality content on the sub-module ensuring the response is sufficiently detailed covering all the relevant topics related to the sub-module. You will also \
+be provided with my course requirements and needs. Structure the course according to my needs.
 
-  content_generation_prompt = """I'm seeking your expertise on the subject of {sub_module_name}. You have access to the subject's information which you have to use while generating \
-a detailed and informative description that covers essential aspects such as definition, \
-explanation, use cases, applications, and any other relevant details. \
-Ensure that the content exceeds 800 words to offer a thorough understanding of the topic.
+MY COURSE REQUIREMENTS : {profile}
 
-SUBJECT INFORMATION : {search_result}
+SUBJECT INFORMATION : {context}
 
-In your response, consider breaking down the information into subsections for clarity. \
-If there are specific examples or real-world applications related to the subject, \
-please include them to enhance practical understanding. Additionally, conclude your \
-response by suggesting relevant URLs for further reading to empower users with \
-additional resources on the subject. Make sure your output is a valid json where the keys are the subject_name, \
-title_for_the_content, content, subsections (which should be a list of dictionaries with the keys - title and content) and urls (which should be a list).
-"""
-  
+--------------------------------
+In your response, organize the information into subsections for clarity and elaborate on each subsection with suitable examples if and only if it is necessary. \
+If applicable, incorporate real-world examples, applications or use-cases to illustrate the relevance of the topic in various contexts. Additionally, incorporate anything \
+that helps the student to better understand the topic. \
+Please format your output as valid JSON, with the following keys: title_for_the_content (suitable title for the sub-module), \
+content(the main content of the sub-module), subsections (a list of dictionaries with keys - title and content).
+Be a good educational assistant and craft the best way to explain the sub-module following my course requirement strictly..
+  """
 
+    all_content = []
+    flag = 1 if api_key_to_use== 'first' else (2 if api_key_to_use=='second' else 3 )
+    print(f'THREAD {flag} RUNNING...')
+    openai_api_key = openai_api_key1 if flag == 1 else(openai_api_key2 if flag == 2 else openai_api_key3)
+    for key,val in output.items():
+        relevant_docs = vectordb.similarity_search(val)
+        rel_docs = [doc.page_content for doc in relevant_docs]
+        context = '\n'.join(rel_docs)
 
-  content_generation_prompt = """You are an educational assistant with knowledge in various domains. You will be provided with context from a textbook \
-  and your task is to design informative course explaining all the concepts in depth in the textbook. Your main goal is to use the subject's information which you have to use while generating \
-  a detailed and informative description that covers essential aspects such as definition, \
-  explanation, use cases, applications, and any other relevant details. \
-  Ensure that the content exceeds 800 words to offer a thorough understanding of the topic.
+        client = OpenAI(api_key= openai_api_key)
 
-Module Name: {module_name}
+        completion = client.chat.completions.create(
+                    model = 'gpt-3.5-turbo-1106',   
+                    messages = [
+                        {'role':'user', 'content': prompt.format(sub_module_name = val, module_name = module_name, profile= profile, context=context)},
+                    ],
+                    response_format = {'type':'json_object'},
+                    seed = 42
+        )
+        print("Thread 1: Module Generated: ",key,"!")   
+        content_output = ast.literal_eval(completion.choices[0].message.content)
+        content_output['subject_name'] = val
+        print(content_output)
+        all_content.append(content_output)
 
-Context: {context}
-
-Follow the provided JSON format diligently.
-"""
-
-  client = OpenAI(api_key=openai_api_key1)
-  completion = client.chat.completions.create(
-          model = 'gpt-3.5-turbo-1106',
-          messages = [
-              {'role':'user', 'content': content_generation_prompt.format(module_name = module_name, context = context)},
-          ],
-          response_format = {'type':'json_object'},
-          seed = 42,
-)
-  output = ast.literal_eval(completion.choices[0].message.content)
-
-  return output
+    return all_content
