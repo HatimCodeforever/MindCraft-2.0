65d323c54a483f77416c8232b44a81c401a41e6c
Haadi Rakhangi <127193364+haadirakhangi@users.noreply.github.com>
2024-04-13 10:20:58 +0530

    merge

--- client/src/views/Sildeshow.tsx
+++ client/src/views/Sildeshow.tsx
@@ -1,27 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { Image } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Image, Button } from "@chakra-ui/react";
+
 const Slideshow = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    }, 8000); // Change image every 3 seconds
+  const goToNextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
 
-    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
-  }, [images.length]);
+  const goToPreviousImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
 
   return (
-    <div style={{"padding": "10px"}}>
+    <div style={{ padding: "10px" }}>
       {images.map((image, index) => (
         <Image
           key={index}
           src={image}
           boxSize={{ base: '50px', md: '100px', lg: '500px' }}
           alt={`Slide ${index}`}
-          style={{ display: index === currentIndex ? 'block' : 'none'}}
+          style={{ display: index === currentIndex ? 'block' : 'none' }}
         />
       ))}
+      <Button mt={2} onClick={goToPreviousImage} color={"white"}
+        rounded="md"
+        _hover={{
+          transform: "translateY(-2px)",
+          boxShadow: "md",
+          transition: "transform 0.3s ease",  // Add transition property for smooth transition
+          bg: "purple.600"
+        }}
+        bg={"purple.400"}>Previous</Button>
+      <Button mt={2} style={{ "float": "right" }} color={"white"}
+        rounded="md"
+        _hover={{
+          transform: "translateY(-2px)",
+          boxShadow: "md",
+          transition: "transform 0.3s ease",  // Add transition property for smooth transition
+          bg: "purple.600"
+        }}
+        bg={"purple.400"} onClick={goToNextImage}>Next</Button>
     </div>
   );
 };
